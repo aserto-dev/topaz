e7ac8f8b75d9bc16b93f0184bf8c556403bb9ab0
Ronen Hilewicz <ronen@aserto.com>
2022-11-23 10:38:52 -0500

    Allow users to be resolved by ID

--- directory/identity.go
+++ directory/identity.go
@@ -2,20 +2,19 @@ package directory
 
 import (
 	"context"
+	"errors"
 
+	cerr "github.com/aserto-dev/errors"
 	"github.com/aserto-dev/go-authorizer/pkg/aerr"
 	v2 "github.com/aserto-dev/go-directory/aserto/directory/common/v2"
 	ds2 "github.com/aserto-dev/go-directory/aserto/directory/reader/v2"
-	"github.com/google/uuid"
+	"github.com/aserto-dev/go-directory/pkg/derr"
 )
 
 func GetIdentityV2(client ds2.ReaderClient, ctx context.Context, identity string) (*v2.Object, error) {
 	identityString := "identity"
 	obj := v2.ObjectIdentifier{Type: &identityString, Key: &identity}
-	_, err := uuid.Parse(identity)
-	if err == nil {
-		obj = v2.ObjectIdentifier{Id: &identity}
-	}
+
 	relationString := "identifier"
 	subjectType := "user"
 	withObjects := true
@@ -28,15 +27,16 @@ func GetIdentityV2(client ds2.ReaderClient, ctx context.Context, identity string
 		},
 		WithObjects: &withObjects,
 	})
-	if err != nil {
-		return nil, err
-	}
-
-	if relResp.Results == nil {
+	switch {
+	case err != nil && errors.Is(cerr.UnwrapAsertoError(err), derr.ErrNotFound):
 		return nil, aerr.ErrDirectoryObjectNotFound
-	}
+	case err != nil:
+		return nil, err
 
-	if len(relResp.Objects) == 0 {
+	case relResp.Results == nil:
+		return nil, aerr.ErrDirectoryObjectNotFound
+
+	case len(relResp.Objects) == 0:
 		return nil, aerr.ErrDirectoryObjectNotFound.Msg("no objects found in relation")
 	}
 
