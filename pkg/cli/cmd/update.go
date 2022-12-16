75d9b2b176f37f74a148f35b13c61389cefa6199
Gert Drapers <gert.drapers@live.com>
2022-12-16 12:01:48 -0800

    add update cmd

---
+++ pkg/cli/cmd/update.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"github.com/aserto-dev/topaz/pkg/cli/cc"
+	"github.com/aserto-dev/topaz/pkg/cli/dockerx"
+	"github.com/fatih/color"
+)
+
+type UpdateCmd struct {
+	ContainerName    string `optional:"" default:"topaz" help:"container name"`
+	ContainerVersion string `optional:"" default:"latest" help:"container version" `
+	Hostname         string `optional:"" help:"hostname for docker to set"`
+}
+
+func (cmd UpdateCmd) Run(c *cc.CommonCtx) error {
+	color.Green(">>> updating topaz...")
+
+	args := []string{}
+	args = append(args, "pull")
+	args = append(args, platform...)
+	if cmd.Hostname != "" {
+		args = append(args, hostname...)
+	}
+	args = append(args, containerName...)
+
+	return dockerx.DockerWith(map[string]string{
+		"CONTAINER_NAME":    cmd.ContainerName,
+		"CONTAINER_VERSION": cmd.ContainerVersion,
+	},
+		args...,
+	)
+}
