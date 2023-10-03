38d249666135dc8e653241414357ff42947b9f34
Ronen Hilewicz <ronen@aserto.com>
2023-10-03 07:58:23 -0400

    Replace 0.0.0.0 with localhost

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -21,7 +21,7 @@ const containerImage string = "topaz"
 func init() {
 	os.Setenv("GO_VERSION", "1.20")
 	os.Setenv("DOCKER_BUILDKIT", "1")
-	os.Setenv("CONSOLE_VERSION", "0.8.66-20231002144058.64.g51d1cc0c.v3")
+	os.Setenv("CONSOLE_VERSION", "0.0.0-20231002232018.0.gef5db259")
 }
 
 // Generate generates all code.
