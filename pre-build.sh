4f853dd4b1743dc44e3ce8081abc8d935bee1257
oanatmaria <oana@aserto.com>
2023-10-04 14:29:29 +0300

    Only copy the files of the console build folder

--- pre-build.sh
+++ pre-build.sh
@@ -2,5 +2,5 @@
 
 docker pull ghcr.io/aserto-dev/self-hosted-console:$1
 id=$(docker create ghcr.io/aserto-dev/self-hosted-console:$1)
-docker cp $id:/app/build ./pkg/app/console
+docker cp $id:/app/build/. ./pkg/app/console
 docker rm -v $id
