26ca95900ebd15460b1bef39f0c263469fa9874c
Ronen Hilewicz <ronen@aserto.com>
2023-10-16 18:05:07 -0400

    clean  pkg/app/console before build

--- pre-build.sh
+++ pre-build.sh
@@ -2,5 +2,6 @@
 
 docker pull ghcr.io/aserto-dev/self-hosted-console:$1
 id=$(docker create ghcr.io/aserto-dev/self-hosted-console:$1)
+rm -rf ./pkg/app/console
 docker cp $id:/app/build/. ./pkg/app/console
 docker rm -v $id
