d04ea28476ead88d8304701f4662b925270f802f
carabasdaniel <dani@aserto.com>
2024-01-23 12:11:15 +0200

    Add named configurations

--- pkg/cli/cmd/cli.go
+++ pkg/cli/cmd/cli.go
@@ -18,27 +18,27 @@ const (
 )
 
 type CLI struct {
-	Start     StartCmd     `cmd:"" help:"start topaz in daemon mode"`
-	Stop      StopCmd      `cmd:"" help:"stop topaz instance"`
-	Restart   RestartCmd   `cmd:"" help:"restart topaz instance"`
-	Status    StatusCmd    `cmd:"" help:"status of topaz daemon process"`
-	Run       RunCmd       `cmd:"" help:"run topaz in console mode"`
-	Manifest  ManifestCmd  `cmd:"" help:"manifest commands"`
-	Test      TestCmd      `cmd:"" help:"test assertions commands"`
-	Templates TemplateCmd  `cmd:"" help:"template commands"`
-	Console   ConsoleCmd   `cmd:"" help:"open console in the browser"`
-	Import    ImportCmd    `cmd:"" help:"import directory objects"`
-	Export    ExportCmd    `cmd:"" help:"export directory objects"`
-	Backup    BackupCmd    `cmd:"" help:"backup directory data"`
-	Restore   RestoreCmd   `cmd:"" help:"restore directory data"`
-	Install   InstallCmd   `cmd:"" help:"install topaz container"`
-	Configure ConfigureCmd `cmd:"" help:"configure topaz service"`
-	Certs     CertsCmd     `cmd:"" help:"cert commands"`
-	Update    UpdateCmd    `cmd:"" help:"update topaz container version"`
-	Uninstall UninstallCmd `cmd:"" help:"uninstall topaz container"`
-	Load      LoadCmd      `cmd:"" help:"load manifest from file (DEPRECATED)" hidden:""`
-	Save      SaveCmd      `cmd:"" help:"save manifest to file  (DEPRECATED)" hidden:""`
-	Version   VersionCmd   `cmd:"" help:"version information"`
-	NoCheck   bool         `name:"no-check" short:"N" env:"TOPAZ_NO_CHECK" help:"disable local container status check"`
+	Start             StartCmd     `cmd:"" help:"start topaz in daemon mode"`
+	Stop              StopCmd      `cmd:"" help:"stop topaz instance"`
+	Status            StatusCmd    `cmd:"" help:"status of topaz daemon process"`
+	Run               RunCmd       `cmd:"" help:"run topaz in console mode"`
+	Manifest          ManifestCmd  `cmd:"" help:"manifest commands"`
+	Test              TestCmd      `cmd:"" help:"test assertions commands"`
+	Templates         TemplateCmd  `cmd:"" help:"template commands"`
+	Console           ConsoleCmd   `cmd:"" help:"open console in the browser"`
+	Import            ImportCmd    `cmd:"" help:"import directory objects"`
+	Export            ExportCmd    `cmd:"" help:"export directory objects"`
+	Backup            BackupCmd    `cmd:"" help:"backup directory data"`
+	Restore           RestoreCmd   `cmd:"" help:"restore directory data"`
+	Install           InstallCmd   `cmd:"" help:"install topaz container"`
+	Configure         ConfigureCmd `cmd:"" help:"configure topaz service"`
+	Certs             CertsCmd     `cmd:"" help:"cert commands"`
+	Update            UpdateCmd    `cmd:"" help:"update topaz container version"`
+	Uninstall         UninstallCmd `cmd:"" help:"uninstall topaz container"`
+	Load              LoadCmd      `cmd:"" help:"load manifest from file (DEPRECATED)"`
+	Save              SaveCmd      `cmd:"" help:"save manifest to file  (DEPRECATED)"`
+	Version           VersionCmd   `cmd:"" help:"version information"`
+	NoCheck           bool         `name:"no-check" json:"noCheck,omitempty" short:"N" env:"TOPAZ_NO_CHECK" help:"disable local container status check"`
 	LogLevel  int          `name:"log" short:"L" type:"counter" default:"0" help:"log level"`
+	DefaultConfigFile string       `name:"config-file" json:"configFile,omitempty" default:"config.yaml" short:"c" help:"use topaz configuration file"`
 }
