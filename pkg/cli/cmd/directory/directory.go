45c80860f3354ed32a91758d89101c739780d805
carabasdaniel <dani@aserto.com>
2024-03-04 17:19:31 +0200

    Rework check directory command

--- pkg/cli/cmd/directory/directory.go
+++ pkg/cli/cmd/directory/directory.go
@@ -8,17 +8,16 @@ const (
 )
 
 type DirectoryCmd struct {
-	Import          ImportCmd          `cmd:"" help:"import data into directory"`
-	Export          ExportCmd          `cmd:"" help:"export directory data"`
-	GetObject       GetObjectCmd       `cmd:"" help:"get object" group:"directory"`
-	SetObject       SetObjectCmd       `cmd:"" help:"set object" group:"directory"`
-	DeleteObject    DeleteObjectCmd    `cmd:"" help:"delete object" group:"directory"`
-	ListObjects     ListObjectsCmd     `cmd:"" help:"list objects" group:"directory"`
-	GetRelation     GetRelationCmd     `cmd:"" help:"get relation" group:"directory"`
-	SetRelation     SetRelationCmd     `cmd:"" help:"set relation" group:"directory"`
-	DeleteRelation  DeleteRelationCmd  `cmd:"" help:"delete relation" group:"directory"`
-	ListRelations   ListRelationsCmd   `cmd:"" help:"list relations" group:"directory"`
-	CheckRelation   CheckRelationCmd   `cmd:"" help:"check relation" group:"directory"`
-	CheckPermission CheckPermissionCmd `cmd:"" help:"check permission" group:"directory"`
-	GetGraph        GetGraphCmd        `cmd:"" help:"get relation graph" group:"directory"`
+	Import         ImportCmd         `cmd:"" help:"import data into directory"`
+	Export         ExportCmd         `cmd:"" help:"export directory data"`
+	GetObject      GetObjectCmd      `cmd:"" help:"get object" group:"directory"`
+	SetObject      SetObjectCmd      `cmd:"" help:"set object" group:"directory"`
+	DeleteObject   DeleteObjectCmd   `cmd:"" help:"delete object" group:"directory"`
+	ListObjects    ListObjectsCmd    `cmd:"" help:"list objects" group:"directory"`
+	GetRelation    GetRelationCmd    `cmd:"" help:"get relation" group:"directory"`
+	SetRelation    SetRelationCmd    `cmd:"" help:"set relation" group:"directory"`
+	DeleteRelation DeleteRelationCmd `cmd:"" help:"delete relation" group:"directory"`
+	ListRelations  ListRelationsCmd  `cmd:"" help:"list relations" group:"directory"`
+	Check          CheckCmd          `cmd:"" help:"check" group:"directory"`
+	GetGraph       GetGraphCmd       `cmd:"" help:"get relation graph" group:"directory"`
 }
