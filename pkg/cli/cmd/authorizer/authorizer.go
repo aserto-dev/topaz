4286d4acbc9e1186ea4bd4e8ebcd0d051a1d4d5d
Gert Drapers <gert.drapers@live.com>
2024-05-17 17:05:41 -0700

    az cmd ordering

--- pkg/cli/cmd/authorizer/authorizer.go
+++ pkg/cli/cmd/authorizer/authorizer.go
@@ -1,10 +1,10 @@
 package authorizer
 
 type AuthorizerCmd struct {
-	Test          TestCmd         `cmd:"" help:"execute authorizer assertions"`
 	CheckDecision EvalCmd         `cmd:"" name:"eval" help:"evaluate policy decision"`
-	DecisionTree  DecisionTreeCmd `cmd:"" name:"decisiontree" help:"get decision tree"`
 	ExecQuery     QueryCmd        `cmd:"" name:"query" help:"execute query"`
+	DecisionTree  DecisionTreeCmd `cmd:"" name:"decisiontree" help:"get decision tree"`
 	GetPolicy     GetPolicyCmd    `cmd:"" help:"get policy"`
 	ListPolicies  ListPoliciesCmd `cmd:"" help:"list policies"`
+	Test          TestCmd         `cmd:"" help:"execute authorizer assertions"`
 }
