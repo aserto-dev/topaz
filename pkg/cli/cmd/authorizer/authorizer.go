7839e3c5696a32472a8c11dc57fab1872093ca6c
Gert Drapers <gert.drapers@live.com>
2024-06-27 17:57:13 -0700

    upd az cmds to get policy and list policies

--- pkg/cli/cmd/authorizer/authorizer.go
+++ pkg/cli/cmd/authorizer/authorizer.go
@@ -4,7 +4,15 @@ type AuthorizerCmd struct {
 	CheckDecision EvalCmd         `cmd:"" name:"eval" help:"evaluate policy decision"`
 	ExecQuery     QueryCmd        `cmd:"" name:"query" help:"execute query"`
 	DecisionTree  DecisionTreeCmd `cmd:"" name:"decisiontree" help:"get decision tree"`
-	GetPolicy     GetPolicyCmd    `cmd:"" help:"get policy"`
-	ListPolicies  ListPoliciesCmd `cmd:"" help:"list policies"`
+	Get           GetCmd          `cmd:"" help:"get policy"`
+	List          ListCmd         `cmd:"" help:"list policy"`
 	Test          TestCmd         `cmd:"" help:"execute authorizer assertions"`
+}
+
+type GetCmd struct {
+	Policy GetPolicyCmd `cmd:"" help:"get policy"`
+}
+
+type ListCmd struct {
+	Policies ListPoliciesCmd `cmd:"" help:"list policies"`
 }
