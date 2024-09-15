97946e1f769abda341ed2c2b4511893b8a42808c
Gert Drapers <1533850+gertd@users.noreply.github.com>
2024-09-15 19:43:48 +0200

    decisions

--- check2decision.sh
+++ check2decision.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 convertCheck2Decision() {
-  check2decision -i "${asset}assertions.json" | jq '.assertions | map(del(.check_decision.policy_instance.instance_label))' > "${asset}decisions.json"
+  check2decision -i "${asset}assertions.json" -o "${asset}decisions.json"
 }
 
 assets=("assets/api-auth/test/api-auth_" "assets/gdrive/test/gdrive_" "assets/github/test/github_" "assets/multi-tenant/test/multi-tenant_" "assets/slack/test/slack_" )
