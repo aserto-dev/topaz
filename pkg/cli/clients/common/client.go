d738f85620c4415717e23ddedf8b54831c46315f
Gert Drapers <1533850+gertd@users.noreply.github.com>
2024-11-27 09:31:05 -0800

    add validate placeholder

---
+++ pkg/cli/clients/common/client.go
@@ -0,0 +1,11 @@
+package common
+
+import (
+	"context"
+
+	client "github.com/aserto-dev/go-aserto"
+)
+
+func Validate(ctx context.Context, cfg *client.Config) (bool, error) {
+	return false, nil
+}
