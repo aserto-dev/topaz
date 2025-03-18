c92f5b0ca42b2447d52098e872427512c0f06a85
Gert Drapers <1533850+gertd@users.noreply.github.com>
2025-03-18 16:49:10 -0700

    fix FileExist add not directory clause

--- pkg/cc/config/config.go
+++ pkg/cc/config/config.go
@@ -231,7 +231,7 @@ func (c *Config) setupCerts(log *zerolog.Logger, certsGenerator *certs.Generator
 }
 
 func FileExists(path string) (bool, error) {
-	if _, err := os.Stat(path); err == nil {
+	if fi, err := os.Stat(path); err == nil && !fi.IsDir() {
 		return true, nil
 	} else if os.IsNotExist(err) {
 		return false, nil
