782acbcd90151c29e94122c38de4dba69a551f26
Gert Drapers <1533850+gertd@users.noreply.github.com>
2025-12-15 15:55:01 +0100

    sync main from v34 (#644)

--- pkg/cli/x/dir.go
+++ pkg/cli/x/dir.go
@@ -3,6 +3,7 @@ package x
 // Directory constants.
 const (
 	DefCertsDir     string = "/certs"
+	DefCfgDir       string = "/config"
 	DefDBDir        string = "/data"
 	DefDecisionsDir string = "/decisions"
 )
