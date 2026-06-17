1ae48e2743396dfcd629cdb35ee78742fc979196
Gert Drapers <1533850+gertd@users.noreply.github.com>
2026-06-17 17:26:38 +0200

    impl JSONL exporter

---
+++ topaz/clients/directory/exporter.go
@@ -0,0 +1,53 @@
+package directory
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	dse "github.com/aserto-dev/go-directory/aserto/directory/exporter/v3"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func (c *Client) ExportToFile(ctx context.Context, w io.Writer, options uint32) error {
+	stream, err := c.Exporter.Export(ctx, &dse.ExportRequest{
+		Options:   options,
+		StartFrom: &timestamppb.Timestamp{},
+	})
+	if err != nil {
+		return err
+	}
+
+	encoder := json.NewEncoder(w)
+
+	for {
+		msg, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			break
+		}
+
+		if err != nil {
+			return err
+		}
+
+		switch m := msg.GetMsg().(type) {
+		case *dse.ExportResponse_Object:
+			if err := encoder.Encode(m.Object); err != nil {
+				return err
+			}
+
+		case *dse.ExportResponse_Relation:
+			if err := encoder.Encode(m.Relation); err != nil {
+				return err
+			}
+
+		default:
+			fmt.Fprintf(os.Stderr, "unknown message type\n")
+		}
+	}
+
+	return nil
+}
--- topaz/cmd/directory/export.go
+++ topaz/cmd/directory/export.go
@@ -1,9 +1,97 @@
 package directory
 
-import dsc "github.com/aserto-dev/topaz/topaz/clients/directory"
+import (
+	"context"
+	"os"
+	"path/filepath"
+
+	dse "github.com/aserto-dev/go-directory/aserto/directory/exporter/v3"
+	"github.com/aserto-dev/topaz/internal/fs"
+	"github.com/aserto-dev/topaz/topaz/cc"
+	"github.com/aserto-dev/topaz/topaz/clients"
+	dsc "github.com/aserto-dev/topaz/topaz/clients/directory"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+const (
+	expObjects   string = "obj"
+	expRelations string = "rel"
+	expAll       string = "all"
+)
 
 type ExportCmd struct {
 	dsc.Config
 
-	File string `flag:"file" short:"f" type:"path" help:"path to target export file"`
+	File   string `flag:"file" short:"f" type:"path" help:"path to target export file"`
+	Stdout bool   `flag:"stdout" help:"output to stdout" xor:"file,stdout" required:""`
+	Export string `flag:"export" short:"x" enum:"obj,rel,all" default:"all" help:"export [obj|rel|all] types" required:""`
+}
+
+func (cmd *ExportCmd) Run(ctx context.Context) error {
+	if err := cmd.checkPath(); err != nil {
+		return err
+	}
+
+	if ok, err := clients.Validate(ctx, &cmd.Config); !ok {
+		return err
+	}
+
+	dsClient, err := dsc.NewClient(ctx, &cmd.Config)
+	if err != nil {
+		return err
+	}
+
+	cc.Con().Info().Msg(">>> exporting data to %q", cmd.File)
+
+	var writer *os.File
+
+	if cmd.Stdout {
+		writer = os.Stdout
+	} else {
+		writer, err = os.Create(cmd.File)
+		if err != nil {
+			return err
+		}
+		defer writer.Close()
+	}
+
+	return dsClient.ExportToFile(ctx, writer, cmd.opts())
+}
+
+func (cmd *ExportCmd) checkPath() error {
+	if cmd.File == "" {
+		cmd.Stdout = true
+		cmd.File = "stdout"
+	}
+
+	if cmd.Stdout {
+		return nil
+	}
+
+	dir, _ := filepath.Split(cmd.File)
+
+	ok, err := fs.DirExistsEx(dir)
+	if ok && err == nil {
+		return nil
+	}
+
+	if !ok && err == nil {
+		return status.Errorf(codes.NotFound, "directory %q not found", dir)
+	}
+
+	return status.Error(codes.Unknown, err.Error())
+}
+
+func (cmd *ExportCmd) opts() uint32 {
+	switch cmd.Export {
+	case expObjects:
+		return uint32(dse.Option_OPTION_DATA_OBJECTS)
+	case expRelations:
+		return uint32(dse.Option_OPTION_DATA_RELATIONS)
+	case expAll:
+		fallthrough
+	default:
+		return uint32(dse.Option_OPTION_DATA)
+	}
 }
