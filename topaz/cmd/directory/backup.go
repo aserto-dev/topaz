ed73e9f241a088c51e10818e4c53dc90258b047f
Gert Drapers <1533850+gertd@users.noreply.github.com>
2026-06-18 15:20:53 +0200

    switch back/restore to arg for backup file

--- topaz/cmd/directory/backup.go
+++ topaz/cmd/directory/backup.go
@@ -5,5 +5,5 @@ import dsc "github.com/aserto-dev/topaz/topaz/clients/directory"
 type BackupCmd struct {
 	dsc.Config
 
-	File string `flag:"file" short:"f" type:"path" help:"path to target backup file"`
+	File string `arg:"" default:"backup.tar.gz" help:"path to target backup file"`
 }
--- topaz/cmd/directory/restore.go
+++ topaz/cmd/directory/restore.go
@@ -5,5 +5,5 @@ import dsc "github.com/aserto-dev/topaz/topaz/clients/directory"
 type RestoreCmd struct {
 	dsc.Config
 
-	File string `flag:"file" short:"f" type:"path" help:"path to source backup file"`
+	File string `arg:"" default:"backup.tar.gz" help:"path to source backup file"`
 }
