225b03cfdae4589b6090b1a535fda26bc9315705
Gert Drapers <1533850+gertd@users.noreply.github.com>
2026-08-04 11:58:49 +0200

    fix template

--- pkg/config/templates.go
+++ pkg/config/templates.go
@@ -52,10 +52,11 @@ remote_directory:
 # default jwt validation configuration
 jwt:
   acceptable_time_skew_seconds: 5 # set as default, 5 secs
-  allowed_configuration_endpoints: # NOTE: if empty, any issuer is accepted !!!
-  #  - "https://issuer.example.com/.well-known/openid-configuration"
+  allowed_issuers: # NOTE: if empty, any issuer is accepted !!!
+  #  - "https://issuer.example.com/"
   cache_refresh_min_interval: 5m  # set as default, 5 minutes
   cache_refresh_max_interval: 15m # set as default, 15 minutes
+  expected_audience: "" # set ad default, empty
 
 # authentication configuration
 auth:
