765c4ebe72f43a45e6bc26719c2a6cc1efe5cfe1
Gert Drapers <1533850+gertd@users.noreply.github.com>
2026-09-21 14:25:40 +0200

    add service HTTP endpoints, OpenAPIendpoints  and AuthZEN Access service WellKnownConfigURL

--- topaz/cmd/configure/info.go
+++ topaz/cmd/configure/info.go
@@ -4,12 +4,17 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"net/url"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/aserto-dev/topaz/internal/xdg"
+	"github.com/aserto-dev/topaz/pkg/config"
 	"github.com/aserto-dev/topaz/topaz/cc"
 	"github.com/aserto-dev/topaz/topaz/cmd/common"
+	"github.com/aserto-dev/topaz/topazd/service/builder"
+	"github.com/samber/lo"
 
 	"github.com/itchyny/gojq"
 )
@@ -26,7 +31,7 @@ func (cmd InfoConfigCmd) Run(ctx context.Context) error {
 
 	// use Info struct when output all, to preserve ordering of root objects.
 	if cmd.Var == "" {
-		return enc.Encode(cmd.info())
+		return enc.Encode(cmd.GetInfo())
 	}
 
 	query, err := gojq.Parse("." + cmd.Var)
@@ -91,20 +96,34 @@ type Info struct {
 		NoColor           bool   `json:"topaz_no_color"`
 	} `json:"default"`
 	Directory struct {
-		DirectorySvc   string `json:"topaz_directory_svc"`
-		DirectoryKey   string `json:"topaz_directory_key"`
-		DirectoryToken string `json:"topaz_directory_token"`
-		Insecure       bool   `json:"topaz_insecure"`
+		DirectorySvc     string `json:"topaz_directory_svc"`
+		DirectorySvcHttp string `json:"topaz_directory_svc_http"`
+		DirectoryKey     string `json:"topaz_directory_key"`
+		DirectoryToken   string `json:"topaz_directory_token"`
+		Insecure         bool   `json:"topaz_insecure"`
+		Plaintext        bool   `json:"topaz_plaintext"`
+		Timeout          string `json:"timeout"`
 	} `json:"directory"`
 	Authorizer struct {
-		AuthorizerSvc   string `json:"topaz_authorizer_svc"`
-		AuthorizerKey   string `json:"topaz_authorizer_key"`
-		AuthorizerToken string `json:"topaz_authorizer_token"`
-		Insecure        bool   `json:"topaz_insecure"`
+		AuthorizerSvc     string `json:"topaz_authorizer_svc"`
+		AuthorizerSvcHttp string `json:"topaz_authorizer_svc_http"`
+		AuthorizerKey     string `json:"topaz_authorizer_key"`
+		AuthorizerToken   string `json:"topaz_authorizer_token"`
+		Insecure          bool   `json:"topaz_insecure"`
+		Plaintext         bool   `json:"topaz_plaintext"`
+		Timeout           string `json:"timeout"`
 	} `json:"authorizer"`
+	OpenAPI struct {
+		Directory  string `json:"topaz_directory_svc"`
+		Access     string `json:"topaz_access_svc"`
+		Authorizer string `json:"topaz_authorizer_svc"`
+	} `json:"open_api_endpoints"`
+	Access struct {
+		WellKnownConfigURL string `json:"wellknown_config_url"`
+	} `json:"access"`
 }
 
-func (cmd InfoConfigCmd) info() *Info {
+func (cmd InfoConfigCmd) GetInfo() *Info {
 	info := Info{}
 
 	info.Environment.Home = xdg.Home
@@ -126,6 +145,10 @@ func (cmd InfoConfigCmd) info() *Info {
 	info.Runtime.RunningContainerName = cfg.Running.ContainerName
 	info.Runtime.TopazConfigFile = filepath.Join(cc.GetTopazDir(), common.CLIConfigurationFile)
 
+	svcCfg := cmd.svcConfig()
+
+	config.GetConfig(cfg.Running.ConfigFile)
+
 	info.Default.ContainerRegistry = cc.ContainerRegistry()
 	info.Default.ContainerImage = cc.ContainerImage()
 	info.Default.ContainerTag = cc.ContainerTag()
@@ -133,15 +156,27 @@ func (cmd InfoConfigCmd) info() *Info {
 	info.Default.NoCheck = cc.NoCheck()
 	info.Default.NoColor = cc.NoColor()
 
-	info.Directory.DirectorySvc = cc.DirectorySvc()
+	info.Directory.DirectorySvc = grpcEndpoint(svcCfg.APIConfig.Services["reader"])
+	info.Directory.DirectorySvcHttp = httpEndpoint(svcCfg.APIConfig.Services["reader"])
 	info.Directory.DirectoryKey = cc.DirectoryKey()
 	info.Directory.DirectoryToken = cc.DirectoryToken()
 	info.Directory.Insecure = cc.Insecure()
+	info.Directory.Plaintext = cc.Plaintext()
+	info.Directory.Timeout = cc.Timeout().String()
 
-	info.Authorizer.AuthorizerSvc = cc.AuthorizerSvc()
+	info.Authorizer.AuthorizerSvc = grpcEndpoint(svcCfg.APIConfig.Services["authorizer"])
+	info.Authorizer.AuthorizerSvcHttp = httpEndpoint(svcCfg.APIConfig.Services["authorizer"])
 	info.Authorizer.AuthorizerKey = cc.AuthorizerKey()
 	info.Authorizer.AuthorizerToken = cc.AuthorizerToken()
 	info.Authorizer.Insecure = cc.Insecure()
+	info.Authorizer.Plaintext = cc.Plaintext()
+	info.Authorizer.Timeout = cc.Timeout().String()
+
+	info.OpenAPI.Directory = info.Directory.DirectorySvcHttp + "/directory/openapi.json"
+	info.OpenAPI.Access = info.Directory.DirectorySvcHttp + "/access/openapi.json"
+	info.OpenAPI.Authorizer = info.Authorizer.AuthorizerSvcHttp + "/authorizer/openapi.json"
+
+	info.Access.WellKnownConfigURL = info.Directory.DirectorySvcHttp + "/.well-known/authzen-configuration"
 
 	return &info
 }
@@ -149,7 +184,7 @@ func (cmd InfoConfigCmd) info() *Info {
 func (cmd InfoConfigCmd) json() map[string]any {
 	var j map[string]any
 
-	buf, err := json.Marshal(cmd.info())
+	buf, err := json.Marshal(cmd.GetInfo())
 	if err != nil {
 		return map[string]any{}
 	}
@@ -159,4 +194,42 @@ func (cmd InfoConfigCmd) json() map[string]any {
 	}
 
 	return j
+}
+
+func (cmd InfoConfigCmd) svcConfig() *config.Config {
+	cfg := cc.GetConfig()
+
+	if cfg.Running.ConfigFile != "" {
+		c := config.GetConfig(cfg.Running.ConfigFile)
+		return c.Configuration
+	}
+
+	if cfg.Active.ConfigFile != "" {
+		c := config.GetConfig(cfg.Active.ConfigFile)
+		return c.Configuration
+	}
+
+	return nil
+}
+
+func grpcEndpoint(cfg *builder.API) string {
+	return lo.Ternary(cfg.GRPC.FQDN != "", cfg.GRPC.FQDN, cfg.GRPC.ListenAddress)
+}
+
+func httpEndpoint(cfg *builder.API) string {
+	strURL := fmt.Sprintf(
+		"%s://%s",
+		lo.Ternary(cfg.Gateway.HTTP, "http", "https"),
+		strings.Replace(
+			lo.Ternary(cfg.Gateway.FQDN != "", cfg.Gateway.FQDN, cfg.Gateway.ListenAddress),
+			"0.0.0.0", "localhost", 1,
+		),
+	)
+
+	u, err := url.Parse(strURL)
+	if err != nil {
+		return ""
+	}
+
+	return u.String()
 }
